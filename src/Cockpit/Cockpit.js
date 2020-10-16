51fef5e9d60e4f52bd7469832a0046a50020d09f
Grzegorz Kaczor <grzesiek125@op.pl>
2020-10-16 10:10:01 +0200

    components

---
+++ src/Cockpit/Cockpit.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Radium, {StyleRoot} from 'radium';
+
+const cockpit = (props) => {
+        // let classes = ['red', 'bold'].join(' ');  
+        const classes = [];
+
+        if(props.persons.length <= 2){
+          classes.push('red'); // classes = ['red']     w className dodajemy .join(' ')
+        }
+        if (props.persons.length <=1){
+          classes.push('bold');  // classes = ['red', bold]     w className dodajemy .join(' ')
+        }
+
+
+          return(
+              <StyleRoot>
+            <div>
+            <h1>Hi, I'm a React App</h1>
+            <p className={classes.join(' ')} >This is really working!</p>  
+            <button 
+                style={props.style}
+                onClick={props.clicked}>Switch Name</button>
+            </div>
+        </StyleRoot>  
+    );
+};
+
+export default Radium(cockpit);
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import Persons from '../components/Persons/Persons';
 import Radium, {StyleRoot} from 'radium';
-import Person from './Person/Person';
+import Cockpit from '../Cockpit/Cockpit';
 
 class App extends Component {
   state = {
@@ -43,62 +44,49 @@ class App extends Component {
 
 
   render () {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {           // radium psełdoselektor
-        backgroundColor: 'lightgreen',
-        color: 'black',
-      }
-    };
+   let persons = null;
 
-    let persons = null;
-
+   const style = {
+    backgroundColor: 'green',
+    color: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer',
+    ':hover': {           // radium psełdoselektor
+      backgroundColor: 'lightgreen',
+      color: 'black',
+    }
+  };
     if (this.state.showPersons) {
-      persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return <Person 
-            name={person.name} 
-            age={person.age}
-            click={() => this.deletePersonHandler(index)}
-            key={person.id}
-            changed={(event) => this.nameChangedHandler(event, person.id)}
-            />
-          })}
-      </div>
-      )
       style.backgroundColor = 'red';
       style[':hover'] = {         // radium psełdoselektor
         backgroundColor: 'salmon',
         color: 'black'
       }
+      persons = (
+        <div>
+          <Persons 
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler}
+           />
+      </div>
+      );
+
+
     };
-
-    // let classes = ['red', 'bold'].join(' ');  
-    const classes = [];
-
-    if(this.state.persons.length <= 2){
-      classes.push('red'); // classes = ['red']     w className dodajemy .join(' ')
-    }
-    if (this.state.persons.length <=1){
-      classes.push('bold');  // classes = ['red', bold]     w className dodajemy .join(' ')
-    }
     return (
       <StyleRoot>
-        <div className="App">
-          <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(' ')} >This is really working!</p>  
-          <button 
-            style={style}
-            onClick={this.togglePersonsHandler}>Switch Name</button>
-        {persons}
-        </div>
-      </StyleRoot>
+          <div className="App">
+          <Cockpit
+          showPersons={this.state.showPersons}
+          persons={this.state.persons} 
+          clicked={this.togglePersonsHandler}
+          style={style}/>
+          {persons}
+          </div>
+        </StyleRoot>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
